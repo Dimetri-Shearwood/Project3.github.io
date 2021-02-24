8f3aa9488389cf800ec5889651a013ebf26b3075
Dimetri Shearwood <mr.shearwood@Dimetris-MacBook-Air.local>
2021-02-23 21:46:16 -0500

    Did some query selector

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-//document.createElement
+let whole = document.querySelector(".whole")
+let container1 = document.querySelector(".container1")
+let container2 = document.querySelector(".container2")
+let container3 = document.querySelector(".container3")
+let top1 = document.querySelector(".top1")
+let top2 = document.querySelector(".top2")
+let top3 = document.querySelector(".top3")
+let mid1 = document.querySelector(".mid1")
+let mid2 = document.querySelector(".mid2")
+let mid3 = document.querySelector(".mid3")
+let low1 = document.querySelector(".low1")
+let low2 = document.querySelector(".low2")
+let low3 = document.querySelector(".low3")
+
+
 //appendchild
 //do more html in here
 
