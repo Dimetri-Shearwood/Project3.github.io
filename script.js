cabcfe58f20055ba5c2ab7d92556ec82950226a1
Dimetri Shearwood <mr.shearwood@Dimetris-MacBook-Air.local>
2021-02-26 11:41:57 -0500

    Pictures Loaded

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-let whole = document.querySelector(".whole")
-let container1 = document.querySelector(".container1")
-let container2 = document.querySelector(".container2")
-let container3 = document.querySelector(".container3")
-let top1 = document.querySelector(".top1")
-let top2 = document.querySelector(".top2")
-let top3 = document.querySelector(".top3")
-let mid1 = document.querySelector(".mid1")
-let mid2 = document.querySelector(".mid2")
-let mid3 = document.querySelector(".mid3")
-let low1 = document.querySelector(".low1")
-let low2 = document.querySelector(".low2")
-let low3 = document.querySelector(".low3")
-
-
-//appendchild
-//do more html in here
-
-
-
-let url = "https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php/"
-
-
+let url =
+  "https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php/";
 
 fetch(url)
-.then(response=> response.json())
-.then(data=> console.log(data));
+  .then((response) => response.json())
+  .then((data) => createDrinks(data.drinks));
+
+let drinksList = document.querySelectorAll(".drink");
+let imgTarget = document.querySelector("img");
+
+function createDrinks(drinks) {
+  for (let i = 0; i < drinks.length - 1; i++) {
+    drinksList[i].innerHTML = `<img src ="${drinks[i].strDrinkThumb}">`;
+
+        
+  //  <h4>${drinks[i].strInstructions}</h4>`;
+
+    //  console.log(drinksList[i])
+
+    const currentDrink = drinksList[i];
+    currentDrink.addEventListener("click", function (e) {
+      e.preventDefault();
+    //   console.log(drinks[i].strDrinkThumb);
+    imgTarget.src = drinks[i].strDrinkThumb, 
+    imgTarget.style.display = "block";
+    document.querySelector(".instructions").innerHTML = `${drinks[i].strInstructions}`
+
+    });
+  }
+  console.log(drinks);
+}
+let whole = document.getElementById(".whole");
+let container1 = document.getElementById(".container1");
+    imgTarget.addEventListener("click", function (e) {
+        e.preventDefault();
+    imgTarget.style.display = "none";
+    imgTarget.src = "" 
+    
+    })
+
+    //how to style a modal
 
