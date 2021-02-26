4c29f0c24c935c1e5f3d97712f83ff1d3bc1d4f5
Dimetri Shearwood <mr.shearwood@Dimetris-MacBook-Air.local>
2021-02-26 17:07:48 -0500

    Displaying Now

--- script.js
+++ script.js
@@ -7,36 +7,35 @@ fetch(url)
 
 let drinksList = document.querySelectorAll(".drink");
 let imgTarget = document.querySelector("img");
+let modal = document.querySelector(".modal");
 
 function createDrinks(drinks) {
   for (let i = 0; i < drinks.length - 1; i++) {
     drinksList[i].innerHTML = `<img src ="${drinks[i].strDrinkThumb}">`;
 
-        
-  //  <h4>${drinks[i].strInstructions}</h4>`;
+    //  <h4>${drinks[i].strInstructions}</h4>`;
 
     //  console.log(drinksList[i])
 
     const currentDrink = drinksList[i];
     currentDrink.addEventListener("click", function (e) {
       e.preventDefault();
-    //   console.log(drinks[i].strDrinkThumb);
-    imgTarget.src = drinks[i].strDrinkThumb, 
-    imgTarget.style.display = "block";
-    document.querySelector(".instructions").innerHTML = `${drinks[i].strInstructions}`
+      //   console.log(drinks[i].strDrinkThumb);
+      (imgTarget.src = drinks[i].strDrinkThumb),
+        (imgTarget.style.display = "block");
+      document.querySelector(".instructions").innerHTML = `${drinks[i].strInstructions}`;
+      document.querySelector(".name").innerHTML = `${drinks[i].strDrink}`;
+      modal.style.display = "block";
 
+      // document.querySelector(".ingredients").innerHTML = `${drinks[i].strIngredient[i]}`
     });
   }
   console.log(drinks);
 }
 let whole = document.getElementById(".whole");
-let container1 = document.getElementById(".container1");
-    imgTarget.addEventListener("click", function (e) {
-        e.preventDefault();
-    imgTarget.style.display = "none";
-    imgTarget.src = "" 
-    
-    })
-
-    //how to style a modal
-
+let container1 = document.getElementById(".container");
+modal.addEventListener("click", function (e) {
+  e.preventDefault();
+  modal.style.display = "none";
+  imgTarget.src = "";
+});
